2fc2c582c15586b2f315233ffdc028af5c0598b5
SP85691 <suryapratap8562@gmail.com>
2023-04-14 16:03:31 +0530

    New Update

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,22 @@
-from flask import Flask, render_template
-from users import views
+from flask import Flask, render_template, url_for, request
+from .database import db
 
 app = Flask(__name__)
+
+# db.create_all()
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/user/login', methods=['GET', 'POST'])
+def login():
+
+    if request.method == 'POST':
+
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+       
+    return render_template('login.html')
+
